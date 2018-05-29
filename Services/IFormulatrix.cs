2c1dafe14f0cd8177c866df48749b4dc30b63724
tru3d3v <tru3.d3v@gmail.com>
2018-05-29 08:28:57 +0700

    add services

---
+++ Services/IFormulatrix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Services
+{
+    public interface IFormulatrix
+    {
+        void Register(string itemName, string itemContent, int itemType);
+        string Retrieve(string itemName);
+        int GetType(string itemName);
+        void Deregister(string itemName);
+        void Initialize();
+    }
+}
